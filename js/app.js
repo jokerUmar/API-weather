2d03c9bdbb13db3b6d7d51ddb2e988e6dbec419f
jokerUmar <101201795+jokerUmar@users.noreply.github.com>
2022-07-03 16:02:11 +0500

    change1

--- js/app.js
+++ js/app.js
@@ -12,17 +12,17 @@ let renderLocation = function (data1) {
         <p style="font-size: 20px;" >Country: <span class="country-nickname">${data1.sys.country}</span></p>
         <p style="margin-bottom: 5px; margin-top: 5px; font-size: 18px;" class="gradus">${data1.main.temp} °C </p>
         <p style="font-size: 20px;">Speed: <span class="speed">${data1.wind.speed} m/s</span></p>    
-
-    `
-
-    box.insertAdjacentHTML('beforeend' , html)
-    box.style.opacity = "1"
+        <p style="font-size: 18px; margin-top: 5px;">Desc: <span class="desc">${data1.weather[0].description}</span></p>    
+        `
+        box.insertAdjacentHTML('beforeend' , html)
+    
 }
 
 let locationData = async function (location) {
 
     let locationApi = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=277e160f5af509c9f6e384d7cbe3501c`)
     let data = await locationApi.json()
+    console.log(data);
     renderLocation(data)
 }
 
